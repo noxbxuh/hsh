df0be6a7d0f456caa7b8275f3f781229265eae4a
noxbxuh <146133668+noxbxuh@users.noreply.github.com>
2024-03-28 23:05:43 +0300

    Update ham.py

--- ham.py
+++ ham.py
@@ -1,45 +1,40 @@
-import asyncio
-from os import system
-from typing import List
+import telebot 
+import requests 
+from telebot import types
+from time import sleep
+TOKEN = '7035041011:AAFulMQv_03VGP5XTyOrc_ZMUAuwCTztY44' 
+import random
+bot = telebot.TeleBot(TOKEN) 
+num =0
+number = "123456789101113141516171819202123242526272829"
+@bot.message_handler(commands=['st'])
+def send_inline_keyboard(message):
+    global num
+     
+    button1 = types.InlineKeyboardButton(text=f'{num}', callback_data='option1')
+    
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(button1)
+    bot.send_message(chat_id=message.chat.id, text='Number of searches', reply_markup=keyboard)
+  
+@bot.message_handler(commands=['start']) 
+def send_audio(message):
+    global num
+    
+    sleep(1)
+    aa = str(''.join((random.choice(number) for i in range(2))))
+    a = str(''.join((random.choice(number) for i in range(1))))
+    url = f'http://quran.ksu.edu.sa/ayat/mp3/Ali_Jaber_64kbps/0{aa}00{a}.mp3' 
+    response = requests.get(url, verify=False) 
+    if response.status_code == 200: 
+       bot.send_audio(message.chat.id,response.content,caption="for me and my parents
+me : @usaByte",parse_mode="html")
+       num += 1
+       print('Done Send file') 
+    else: 
+        print('Error Send file MP3.') 
+ 
 
-import aiohttp
 
+bot.polling()
 
-def write_file(arg: str) -> None:
-    with open('hits.txt', 'a', encoding='UTF-8') as f:
-        f.write(f'{arg}\n')
-
-
-class Checker:
-    def __init__(self, usernames: List[str]):
-        self.to_check = usernames
-
-    async def _check(self, session: aiohttp.ClientSession, username: str) -> None:
-        async with session.head(f'https://www.tiktok.com/@{username}') as response:
-            if response.status == 200 and len(username) > 2:
-                print(
-                    '%s[UNAVAILABLE] https://www.tiktok.com/@%s%s'
-                    % ('\u001b[31;1m', username, '\u001b[0m')
-                )
-            else:
-                print(
-                    '%s[AVAILABLE] https://www.tiktok.com/@%s%s'
-                    % ('\u001b[32;1m', username, '\u001b[0m')
-                )
-                write_file(username)
-
-    async def start(self):
-        print('Loading.. This may take awhile.')
-        async with aiohttp.ClientSession() as sess:
-            return await asyncio.gather(*[self._check(sess, u) for u in self.to_check])
-
-
-if __name__ == '__main__':
-    system('cls && title TikTok Username Checker by NightfallGT and github.com/sql69')
-
-    with open('usernames.txt', encoding='UTF-8') as f:
-        username_list = [line.strip() for line in f]
-
-    checker = Checker(username_list)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(checker.start())
